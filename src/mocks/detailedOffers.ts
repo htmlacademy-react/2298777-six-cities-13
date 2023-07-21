3faade2e98df49a4f085d9a73a36e013944558c7
Yuriy <originalgamerdaddy@icloud.com>
2023-07-21 15:03:47 +0500

    усложнил мокки???

--- src/mocks/detailedOffers.ts
+++ src/mocks/detailedOffers.ts
@@ -3,6 +3,7 @@ import offers from './offers';
 
 const additionalInfo = [
   {
+    id: '1',
     description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
     bedrooms: 3,
     goods: ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
@@ -19,23 +20,7 @@ const additionalInfo = [
     maxAdults: 4
   },
   {
-    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
-    bedrooms: 3,
-    goods: ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
-    host: {
-      name: 'Angelina',
-      avatarUrl: 'img/avatar-angelina.jpg',
-      isPro: true
-    },
-    images: [
-      'img/apartment-01.jpg',
-      'img/apartment-02.jpg',
-      'img/apartment-03.jpg',
-      'img/room.jpg',
-    ],
-    maxAdults: 4
-  },
-  {
+    id: '2',
     description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
     bedrooms: 3,
     goods: ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
@@ -53,6 +38,25 @@ const additionalInfo = [
     maxAdults: 4
   },
   {
+    id: '3',
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    bedrooms: 3,
+    goods: ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    host: {
+      name: 'Angelina',
+      avatarUrl: 'img/avatar-angelina.jpg',
+      isPro: true
+    },
+    images: [
+      'img/apartment-01.jpg',
+      'img/apartment-02.jpg',
+      'img/apartment-03.jpg',
+      'img/room.jpg',
+    ],
+    maxAdults: 4
+  },
+  {
+    id: '4',
     description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
     bedrooms: 3,
     goods: ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
@@ -68,6 +72,7 @@ const additionalInfo = [
     maxAdults: 4
   },
   {
+    id: '5',
     description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
     bedrooms: 3,
     goods: ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
@@ -84,6 +89,7 @@ const additionalInfo = [
     maxAdults: 4
   },
   {
+    id: '6',
     description: 'luxury skuf apartment',
     bedrooms: 1,
     goods: ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
@@ -100,6 +106,7 @@ const additionalInfo = [
     maxAdults: 2
   },
   {
+    id: '7',
     description: 'luxury skuf room',
     bedrooms: 20,
     goods: ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
@@ -117,9 +124,18 @@ const additionalInfo = [
   },
 ];
 
-const detailedOffers : DetailedOffers = offers.map((offer, index) => ({
-  ...offer,
-  ...additionalInfo[index],
-}));
+const createDetailedOffers = () : DetailedOffers => {
+  const length = Math.min(offers.length, additionalInfo.length);
+  const detailedOffers = [];
+  for (let i = 0; i < length; i++) {
+    const res = additionalInfo.find((offer) => offer.id === offers[i].id);
+    if (res) {
+      detailedOffers.push({ ...offers[i], ...res });
+    }
+  }
+  return detailedOffers;
+};
+
+const detailedOffers : DetailedOffers = createDetailedOffers();
 
 export default detailedOffers;
